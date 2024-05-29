70c7d52dcdb4ba5f9d02fec1729a928f3a3da39b
Ounirihab <rihabouni413@gmail.com>
2024-05-29 13:32:21 +0200

    chores

--- src/app/modules/backoffice/employee-profile/employee-profile.component.ts
+++ src/app/modules/backoffice/employee-profile/employee-profile.component.ts
@@ -1,45 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../../core/auth/auth.service';
 import { Employee } from '../../../shared/models/employee';
 import { EmployeeService } from '../../../core/http/employee.service';
-import { error } from 'console';
+
 @Component({
-  selector: 'app-employee-profile',
-  templateUrl: './employee-profile.component.html',
-  styleUrl: './employee-profile.component.css'
+    selector: 'app-employee-profile',
+    templateUrl: './employee-profile.component.html',
+    styleUrls: ['./employee-profile.component.css']
 })
-export class EmployeeProfileComponent {
+export class EmployeeProfileComponent implements OnInit {
 
+    employeeForm: FormGroup;
+    user: Employee;
 
-  username:string;
-  user:Employee;
-  contactId:number;
-  constructor(
-    private authService:AuthService,
-    private employeeService:EmployeeService
-  ){}
+    constructor(
+        private fb: FormBuilder,
+        private authService: AuthService,
+        private employeeService: EmployeeService
+    ) { }
 
+    ngOnInit(): void {
+        // Initialize the form and fetch employee data
+        this.initializeForm();
+        const username = this.authService.getAuthenticatedUsername();
+        this.fetchEmployeeByUsername(username);
+    }
 
+    initializeForm() {
+        // Define form controls and validators
+        this.employeeForm = this.fb.group({
+            username: [{ value: '', disabled: true }, Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['',],
+            email: [{ value: '', disabled: true }, [Validators.required, Validators.email]],
+            phone: ['', Validators.required],
+            fax: [''],
+            location: ['', Validators.required],
+            modeDePaiement: ['', Validators.required],
+            numCompte: ['', Validators.required],
+            password: [{ value: '', disabled: true }]
+        });
+    }
 
-  ngOnInit(){
-      this.username=this.authService.getAuthenticatedUsername();
-      this.fetchEmployeeByUsername(this.username);
+    fetchEmployeeByUsername(username: string) {
+        // Fetch employee data based on username
+        this.employeeService.getEmployeeByUsername(username).subscribe(
+            (employee: Employee) => {
+                this.user = employee;
+                this.populateForm(employee);
+            },
+            error => {
+                console.error('Error fetching employee data', error);
+            }
+        );
+    }
 
-  }
+    populateForm(employee: Employee) {
+        // Populate form fields with employee data
+        let firstName = '';
+        let lastName = '';
+        const nameParts = employee.name.split(' ');
 
+        if (nameParts.length === 1) {
+            firstName = nameParts[0];
+        } else if (nameParts.length > 1) {
+            firstName = nameParts[0];
+            lastName = nameParts.slice(1).join(' ');
+        }
 
-  fetchEmployeeByUsername(username:string){
-    this.employeeService.getEmployeeByUsername(username).subscribe(
-      (item:Employee)=>{
-        this.user=item;
-        this.contactId=item.contactId;
-        console.log(`Current User ${username} current User Id  ${this.contactId}` )
-        console.log("fetched user in the profule component",this.user)
-      },
-      (error)=>{
-        console.log("Error fetching the employee with the username",error)
-      }    
-    )
-  }
+        this.employeeForm.patchValue({
+            username: employee.username,
+            firstName: firstName,
+            lastName: lastName,
+            email: employee.email,
+            phone: employee.phone,
+            fax: employee.fax,
+            location: employee.location,
+            modeDePaiement: employee.modeDePaiement,
+            numCompte: employee.numCompte,
+            password: employee.password
+        });
+    }
 
+    patchEmployee() {
+        if (this.employeeForm.valid) {
+            const updatedEmployee: Partial<Employee> = {
+                name: `${this.employeeForm.value.firstName} ${this.employeeForm.value.lastName}`,
+                phone: this.employeeForm.value.phone,
+                fax: this.employeeForm.value.fax,
+                location: this.employeeForm.value.location,
+                modeDePaiement: this.employeeForm.value.modeDePaiement,
+                numCompte: this.employeeForm.value.numCompte
+            };
+
+            this.employeeService.patchEmployee(this.user.contactId, updatedEmployee).subscribe(
+                response => {
+                    console.log('Employee updated successfully', response);
+                    // Optionally, you can fetch the updated data again after successful update
+                    // this.fetchEmployeeByUsername(this.user.username);
+                },
+                error => {
+                    console.error('Error updating employee', error);
+                }
+            );
+        } else {
+            // Form is invalid, handle accordingly
+            console.log('Form is invalid');
+        }
+    }
 }
